e8cf3fcf2f7ab90b2a518e5286562dca91d4bad4
Charley <charleylla@foxmail.com>
2020-01-02 09:27:09 +0800

    style-loader 的注释

--- examples/webpack-playground/custom-loader/loaders/style-loader.js
+++ examples/webpack-playground/custom-loader/loaders/style-loader.js
@@ -1,4 +1,8 @@
 const loaderUtils = require('loader-utils')
+// 如果按照顺序调用，source 是一个 module.exports 的 js 脚本
+// 然后我们需要将这段脚本中的 css 提取出来插入到 style 中
+// 我们是无法直接将 module.exports 这样的代码转换为 css 的
+// 或许可以使用 eval
 function styleLoader() { }
 
 styleLoader.pitch = function (remainingRequests) {
@@ -8,6 +12,11 @@ styleLoader.pitch = function (remainingRequests) {
       '!!' + remainingRequests
     )
   )
+  // 这里使用剩下的 loader 先处理样式
+  // require 的是一个文件路径，而不是上面的脚本
+  // 文件路径的脚本运行后才会生成一个 css 样式
+  // 这样就拿到了最终的 css 了
+  // 指定这个脚本返回的是一个 css
   let str = `
     const style = document.createElement('style')
     style.innerHTML = require(${loaderUtils.stringifyRequest(this, '!!' + remainingRequests)})
